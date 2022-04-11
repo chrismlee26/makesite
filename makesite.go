34a107ff23a8c67c9f5c6e02b52c17d6dc5837d8
CHRIS_LEE <chrismlee87@gmail.com>
2022-04-11 15:59:41 -0700

    [test] no such file or directory, bug checkpoint

--- makesite.go
+++ makesite.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"io/ioutil"
 	"os"
+	// "fmt"
 )
 
 type Page struct {
@@ -20,13 +22,43 @@ func check(e error) {
 }
 
 func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	// fileContents, err := ioutil.ReadFile("first-post.txt")
+	// check(err)
+
+	// page := Page{
+	// 	TextFilePath: "./first-post.txt",
+	// 	TextFileName: "First Post",
+	// 	HTMLPagePath: "first-post.html",
+	// 	Content:      string(fileContents),
+	// }
+
+	// t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	// newFile, err := os.Create("first-post.html")
+	// check(err)
+
+	// t.Execute(newFile, page)
+
+	// fmt.Println(string(fileContents))
+
+	// newFile.Close()
+
+	newFile := flag.String("file", "default", "Txt file to read:")
+	flag.Parse()
+	save(*newFile)
+
+	// http.ListenAndServe(":8080", nil)
+}
+
+func save(inputFile string) {
+	fileContents, err := ioutil.ReadFile("./" + inputFile + ".txt")
 	check(err)
+	outputFile := inputFile + ".html"
 
 	page := Page{
-		TextFilePath: "./first-post.txt",
-		TextFileName: "First Post",
-		HTMLPagePath: "first-post.html",
+		TextFilePath: "./" + inputFile,
+		TextFileName: inputFile,
+		HTMLPagePath: outputFile,
 		Content:      string(fileContents),
 	}
 
@@ -36,7 +68,5 @@ func main() {
 	check(err)
 
 	t.Execute(newFile, page)
-	newFile.Close()
 
-	// http.ListenAndServe(":8080", nil)
 }
