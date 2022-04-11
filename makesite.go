d9829b7f23d5d7422db14fe3d12fdf5c91d297f9
CHRIS_LEE <chrismlee87@gmail.com>
2022-04-11 02:07:50 -0700

    [feat] add template writing to stdout

--- makesite.go
+++ makesite.go
@@ -1,8 +1,23 @@
 package main
 
 import (
-	"io/ioutil"
+	// "fmt"
+
+	"html/template"
+	"os"
 )
+
+type post struct {
+	Title   string
+	Content string
+}
+
+type Page struct {
+	TextFilePath string
+	TextFileName string
+	HTMLPagePath string
+	Content      string
+}
 
 func check(e error) {
 	if e != nil {
@@ -15,7 +30,22 @@ func main() {
 	// check(err)
 	// fmt.Println(string(content))
 
-	bytesToWrite := []byte("hello\ngo")
-	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
+	// bytesToWrite := []byte("hello\ngo")
+	// err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
+	// check(err)
+
+	fileContents := []byte("A string for demo purposes.")
+	page := Page{
+		TextFilePath: "filePath",
+		TextFileName: "new",
+		HTMLPagePath: "new.html",
+		Content:      string(fileContents),
+	}
+
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	newFile, err := os.Create("new.html")
 	check(err)
+
+	t.Execute(newFile, page)
 }
